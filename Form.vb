0dbe926ce6324b00b8cbed17c6816da3f72a68ad
kennomatt28 <kennomatt@outlook.com>
2016-03-09 10:36:30 +1100

    Update Form.vb

--- Form.vb
+++ Form.vb
@@ -1,6 +1,7 @@
 Public Class Form
   dim f() as sub
   dim tb as new Output
+  dim op as string
   Private Sub Form1_Load(Byval sender as object, byval e as eventargs) handles base.load
     tb.location= new point(20,20)
     tb.font=new font("Arial",20)
