e4b7bf19e13a65af59b43454b5b5f6bf0a619939
kennomatt28 <kennomatt@outlook.com>
2016-03-08 16:55:04 +1100

    Create Output.vb

---
+++ Output.vb
@@ -0,0 +1,11 @@
+Public Class Output
+  Dim t as string
+  Public text as property
+    Get 
+      return t
+    End Get
+    Set(byval value as string)
+      t=value
+    End Set
+  End property
+End Class
