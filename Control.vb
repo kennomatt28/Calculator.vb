0a3621a7090d8d43c2a0ac412d20f4203c540b66
kennomatt28 <kennomatt@outlook.com>
2016-03-08 16:50:07 +1100

    Create Control.vb

---
+++ Control.vb
@@ -0,0 +1,13 @@
+Public Class Control
+ dim text as string
+ dim name as string
+ public metext as property
+  Get
+   return text
+  End Get
+  Set(Byval value as string)
+    text=value
+    name=value
+  End Set
+ end property
+End Class
